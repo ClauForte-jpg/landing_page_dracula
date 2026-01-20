adb1d72f830f84b00ea298cee0c51d47e1867a59
ClauForte-jpg <claudiaforte789@gmail.com>
2026-01-20 14:07:43 -0300

    Arquivos finais prontos para Vercel

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,35 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts(){
-	return gulp.src('./src/scripts/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./dist/js'));
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
 }
 
 function styles(){
-	return gulp.src('./src/styles/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'}))
-		.pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images(){
-	return gulp.src('./src/images/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./dist/images'));
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images, scripts);
+// Esta função leva o seu index da raiz para dentro da DIST
+function comprimeHtml() {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+// Aqui exportamos tudo uma única vez
+exports.default = gulp.parallel(styles, images, scripts, comprimeHtml);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    // O parêntese deve fechar apenas no final da linha:
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts)); 
-};
-
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./index.html', gulp.parallel(comprimeHtml)); 
+};	
